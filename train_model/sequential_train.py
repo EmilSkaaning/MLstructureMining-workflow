6c2970c2c27e38126da4dc33cfd68948721a1184
EmilSkaaning <emil.thyge.kjaer@gmail.com>
2023-08-14 11:24:40 +0100

    Updated Bayes

--- train_model/sequential_train.py
+++ train_model/sequential_train.py
@@ -1,11 +1,9 @@
 import os
 
 if __name__ == '__main__':  # This is a temp file
-    this_dir = '/home/ekjaer/Projects/db/ciff_090_test'
+    this_dir = '/home/ekjaer//Projects/db/structure_finder_2023-07-17T21-24-00-901076'
     n_cpu = 120
-    n_datas = [200, 150, 100, 50]
-    for n_data in n_datas:
-        print(n_data)
-        os.system(f"python train_model.py {this_dir} -n {n_cpu} -s -d {n_data}")
-    
-    #os.system(f"python train_model.py {this_dir} -n {n_cpu}")
+    os.system(f"python train_model.py {this_dir} -n {n_cpu} -s")
+    os.system(f"python train_model.py {this_dir} -n {n_cpu} -s -b")
+
+
